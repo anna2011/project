292ce3ff4f85378636688ea9124bda750d9a179a
ANNE MLANDI <mlandianne@gmail.com>
2018-03-26 13:01:04 +0300

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,29 @@ module.exports = function(grunt) {
                 dest: 'dist/css/library.css'
             },
         },
+        sass: {
+            assets: {
+                files: [{
+                    src: 'css/scss/main.scss',
+                    dest: 'dist/css/main.css'
+                }]
+
+            },
+
+        }
+
 
 
     });
 
     //load plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sass');
 
     //RegisterTasks
     grunt.registerTask('concat-jsLibs', ['concat-jsLibs']);
     grunt.registerTask('concat-jscustom', ['concat-jscustom']);
+    grunt.registerTask('compile-scss', ['sass']);
 
 
 
